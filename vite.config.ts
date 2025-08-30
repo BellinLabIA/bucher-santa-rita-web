04e22e59a4bfb89747a40194c15c650bbb3e1865
BellinLabIA <chantobellin@gmail.com>
2025-08-29 23:18:18 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bucher-santa-rita-web/", // 👈 importantísimo
+  base: "/bucher-santa-rita-web/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
