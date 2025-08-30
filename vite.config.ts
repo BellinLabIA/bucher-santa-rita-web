af4cd90368c635a2240eaabd91927f8182211008
BellinLabIA <chantobellin@gmail.com>
2025-08-29 19:37:44 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,10 @@
-
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bucher-santa-rita-web/",
+  base: "/bucher-santa-rita-web/",   // 👈 EXACTO: tu nombre de repo
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
