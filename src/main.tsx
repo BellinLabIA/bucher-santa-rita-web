435d7307862e7aa33bd833766791b1fa1e8dc1f6
BellinLabIA <chantobellin@gmail.com>
2025-08-29 23:30:21 -0600

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import { createRoot } from "react-dom/client";
-import App from "./App";
+import App from "./App.tsx";
 import "./index.css";
-import { HashRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom"; // 👈 Importamos HashRouter
 
 createRoot(document.getElementById("root")!).render(
   <HashRouter>
