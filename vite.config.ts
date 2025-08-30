826426fd53cc663e71570948649bf1e93a5002e6
BellinLabIA <chantobellin@gmail.com>
2025-08-30 00:50:54 -0600

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,14 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react-swc'
+import path from 'path'
+
 
 export default defineConfig({
   plugins: [react()],
-  base: "/bucher-santa-rita-web/",
+  base: '/bucher-santa-rita-web/',
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      '@': path.resolve(__dirname, './src'),
     },
   },
 });
